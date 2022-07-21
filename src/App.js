29eb37e9f2a00cf31198ec207a4fdde9959c6b6c
bimaap <let.bimaap@gmail.com>
2022-07-21 10:14:06 +0700

    feat: implementation axios

--- src/App.js
+++ src/App.js
@@ -1,47 +1,33 @@
 
 import React from 'react'
-import {Col, Row, Container} from 'react-bootstrap'
+import axios from 'axios'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 const App = () => {
-  let [value, operation] = React.useState(0)
-  return (
-    <Container fluid className='vh-100 d-flex justify-content-center align-items-center'>
-      <Row>
-        <Col className='d-flex align-items-center justify-content-center'>
-          <button onClick={()=>operation(value < 1? 0:value -= 1)} className='btn btn-primary'>-</button>
-          <span className='border border-primar mx-5 my-10 py-1 px-2'>{value}</span>
-          <button onClick={()=>operation(value += 1)} className='btn btn-primary'>+</button>
-        </Col>
-      </Row>
-    </Container>
+  let [data, setData] = React.useState(null)
+
+  React.useEffect(() => {
+    const Auth = async () => {
+      try {
+        const res = await axios.get('https://rickandmortyapi.com/api/character');
+        setData(res.data.results)
+      } catch (err) {
+        console.error(err);
+      }
+    }
+    Auth()
+  }, [])
+
+  return(
+    <main className='vh-100 d-flex flex-row gap-3 justify-content-center align-items-center flex-wrap'>
+      {!data? null:data.map((e) => (
+        <section className='d-flex flex-column justify-content-center align-items-center col-2 p-3 border border-dark rounded'>
+          <img className='col-12' alt='' src={e.image}></img>
+          <span className=''>{e.name}</span>
+        </section>
+      ))}
+    </main>
   )
-}
-
-// class App extends React.Component{
-//   constructor(props) {
-//     super(props)
-//     this.state = {value: 0}
-//   }
-
-//   handleClick(bool){
-//     this.setState(state => ({
-//       value: bool? state.value += 1 : state.value < 1? 0 : state.value -= 1
-//     }))
-//   }
-
-//   render(){
-//     return(
-//       <Container fluid className='vh-100 d-flex justify-content-center align-items-center'>
-//         <Row>
-//           <Col className='d-flex align-items-center'>
-//             <button onClick={()=>this.handleClick(false)} className='btn btn-primary'>-</button>
-//             <span className='border border-primar mx-5 py-1 px-2'>{this.state.value}</span>
-//             <button onClick={()=>this.handleClick(true)} className='btn btn-primary'>+</button>
-//           </Col>
-//         </Row>
-//       </Container>
-//     )
-//   }
-// }
+} 
 
 export default App
